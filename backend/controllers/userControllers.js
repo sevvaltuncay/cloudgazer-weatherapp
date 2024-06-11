7a1efd42ad856b2aa15d0c2bdc5bcb347b6a459f
Şevval Tuncay <sevvaltuncy@gmail.com>
2024-06-11 23:46:25 +0300

    updated

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -48,7 +48,9 @@ const passUser = async function (req, res) {
     const resetLink = `${process.env.CLIENT_URL}?token=${resetToken}`;
 
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
